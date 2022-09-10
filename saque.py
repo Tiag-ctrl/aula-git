e2e46f1dade97f77e18ba7f14cbd678ec666d94e
Tiag_ctrl <tiagossouza@live.com>
2022-09-10 02:48:57 -0300

    teste saque

---
+++ saque.py
@@ -0,0 +1,64 @@
+class Conta:
+	
+    cliente = 'impatech'
+    agencia = '0001'
+    numero_conta = '1010-2'
+    saldo = 0
+
+    def depositar(self, valor):	
+        self.saldo =self.saldo + valor
+        self.extrato(valor)
+
+    def sacar(self, valor):	
+        if valor <= self.saldo:
+            self.saldo =self.saldo -  valor
+            self.extrato( - valor)
+        else:
+            print('Saldo insuficiente.')
+        
+    def menu(self):
+        print('-' * 40)
+        print(f'     MENU DE OPERAÇÕES')
+        print('-' * 40)
+        print('Opções:')
+        print('1 - Extrato')
+        print('2 - Depósito')
+        print('3 - Saque')
+        print('4 - Sair')
+        opcao = input('Digite a opção desejada: ')
+        if opcao == '1':
+            self.extrato()
+        elif opcao == '2':
+            valor = float(input('Digite o valor a ser depositado: '))
+            self.depositar(valor)
+        elif opcao == '3':
+            valor=int(input('Digite o valor a sacar: '))
+            self.sacar(valor)
+        elif opcao == '4':
+            exit()
+
+    def extrato(self, ultimo = 0):
+        print()
+        print('-' * 40)
+        print(f'                EXTRATO           ')
+        print('-' * 40)
+        print(f'CLIENTE - {self.cliente}')
+        print(f'AGÊNCIA {self.agencia} CONTA {self.numero_conta}')
+        print('-' * 40)
+        print('LANÇAMENTOS')
+        if ultimo > 0:
+            print(f' DEPÓSITO  |     CRÉDITO {ultimo:.2F} ')
+        print('              |                |')
+        print('              |                |')
+        print('-' * 40)
+        print(f'                        SALDO: R$ {self.saldo:.2F}')
+        print('-' * 40)
+        print('             FIM DO EXTRATO				  ')
+        print('-' * 40)
+        aperte = input('Aperte ENTER para retornar ao MENU ou (x) para sair: ')
+        if aperte == 'x' or aperte == 'X': 
+            exit()
+        else:
+            self.menu()
+
+pessoa = Conta()
